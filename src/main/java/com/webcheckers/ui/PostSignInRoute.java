e0ce96a312bb7188f53609348d634a41b78e6e81
mxk5025 <mxk5025@rit.edu>
2018-10-04 14:31:23 -0400

    Added sign in to home.ftl

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -6,6 +6,8 @@ import spark.Route;
 
 public class PostSignInRoute implements Route {
 
+    static final String NAME_PARAM = "myName";
+
     @Override
     public Object handle(Request request, Response response) {
         return null;
