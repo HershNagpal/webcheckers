3e524619e0d990e241cd29ffdfc6a079e98b0c0b
matt <mxb9771@rit.edu>
2018-10-04 17:06:36 -0400

    PlayerLobby: stubbed out and imlemented methods PostSignin: created error message if player name is invalid

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,4 +1,36 @@
 package com.webcheckers.appl;
 
+import com.webcheckers.model.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class PlayerLobby {
+
+    private Set<Player> players;
+
+    public PlayerLobby(){
+        players = new HashSet<Player>(){};
+
+    }
+
+    public boolean containsPlayer(Player player){
+        if(player instanceof Player)
+            return players.contains(player);
+        else
+            return false;
+    }
+
+    public Boolean addPlayer(Player player){
+        if(!(player instanceof  Player))return  false;
+        if(containsPlayer(player)) return false;
+        else{
+            players.add(player);
+            return true;
+        }
+
+
+    }
+
+
 }
