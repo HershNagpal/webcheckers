f3879cbec81c7b51c4ebaf9521a042cd1bb3cf52
mxk5025 <michaelkha213@gmail.com>
2018-10-04 20:07:23 -0400

    Documented GetSignInRoute

--- src/main/java/com/webcheckers/ui/GetSignInRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -7,8 +7,14 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
+/**
+ * The UI Controller to GET the Home page.
+ *
+ * @author Michael Kha
+ */
 public class GetSignInRoute implements Route {
 
+    // Values used for rendering the sign in view
     static final String TITLE_ATTR = "title";
     static final String TITLE = "Sign In";
     static final String VIEW_NAME = "signin.ftl";
@@ -17,12 +23,30 @@ public class GetSignInRoute implements Route {
 
     private final TemplateEngine templateEngine;
 
+    /**
+     * Create the Spark Route (UI controller) for the
+     * {@code GET /} HTTP request.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
     public GetSignInRoute(final TemplateEngine templateEngine) {
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
         this.templateEngine = templateEngine;
         LOG.config("GetSignInRoute is initialized.");
     }
 
+    /**
+     * Render the WebCheckers Sign In page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
     @Override
     public Object handle(Request request, Response response) {
         LOG.finer("GetSignInRoute is invoked.");
