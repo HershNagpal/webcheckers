686f30962b65dc77f3dd5a6f721d537de531d8d5
Chrisd319 <cpd7997@rit.edu>
2018-10-05 17:35:04 -0400

    Created the checkColor method in the Space class, in order to determine if a space is black or white based on its rows and columns.

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -9,6 +9,7 @@ public class Space {
     private int column;
     private SpaceType thisSpaceType;
 
+
     public Space(int row, int column){
         this.row = row;
         this.column = column;
@@ -23,4 +24,17 @@ public class Space {
         }
     }
 
+    /**
+     * Checks the color of the space. Returns false for dark square and true for light square.
+     *
+     * @return indicator of color in boolean
+     */
+    public boolean checkColor(){
+        if(row%2==0) {
+            return column % 2 == 0;
+        }
+        else{
+            return column % 2 == 1;
+        }
+    }
 }
