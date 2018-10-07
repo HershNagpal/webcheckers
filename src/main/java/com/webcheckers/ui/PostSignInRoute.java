a2cdb2e6e41aff8abc2ea4578158c5eacfacac7d
lxg8800 <lxg8800@rit.edu>
2018-10-07 16:46:01 -0400

    Added to INVALID_NAME in case user name contains special chars.

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -23,7 +23,8 @@ public class PostSignInRoute implements Route {
     static final String VIEW_NAME = "signin.ftl";
 
     static final String TITLE = "Sign In";
-    static final String INVALID_NAME = "Name must have at least one alphanumeric character.";
+    static final String INVALID_NAME = "Name must have at least one alphanumeric character" +
+            "and contain no special characters.";
     static final String NAME_TAKEN = "Name has already been taken.";
 
     private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
