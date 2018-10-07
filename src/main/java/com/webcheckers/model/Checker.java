0ca8be89b4ccb732bcce42d28e3ace63f2d25890
lxg8800 <lxg8800@rit.edu>
2018-10-07 14:28:48 -0400

    Stubbed Checker.

--- src/main/java/com/webcheckers/model/Checker.java
+++ src/main/java/com/webcheckers/model/Checker.java
@@ -2,4 +2,20 @@ package com.webcheckers.model;
 
 public class Checker {
 
+    public enum PieceType{
+        RED,
+        WHITE
+
+        //TODO: Specify Checker Direction
+    }
+
+    private PieceType type;
+
+    public Checker(){
+
+    }
+
+    public void makeMove(){
+
+    }
 }
