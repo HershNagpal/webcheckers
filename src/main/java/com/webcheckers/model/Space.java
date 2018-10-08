cbdeb036fcaf8f97753ed6abf8ff44ecd0cf3476
lxg8800 <lxg8800@rit.edu>
2018-10-07 21:55:19 -0400

    Added Space isValid() to check if its a valid space to move to.

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -1,80 +1,94 @@
 package com.webcheckers.model;
 
+/**
+ *
+ * @author
+ */
 public class Space {
 
-    /**
-     * KING_RED = Spaces where Red checkers become king pieces.
-     * KING_BLACK = Spaces where Black checkers become king pieces.
-     */
-    public enum SpaceType {KING_RED, KING_BLACK, NON_KING}
+  /**
+   * KING_RED = Spaces where Red checkers become king pieces.
+   * KING_BLACK = Spaces where Black checkers become king pieces.
+   */
+  public enum SpaceType {
+    KING_RED, KING_BLACK, NON_KING
+  }
 
-    public enum SpaceColor {BLACK, WHITE}
+  public enum SpaceColor {BLACK, WHITE}
 
-    private int row;
-    private int column;
-    private SpaceType spaceType;
-    private SpaceColor spaceColor;
+  private int row;
+  private int column;
+  private SpaceType spaceType;
+  private SpaceColor spaceColor;
 
-    //Space has no piece before setUpBoard() in Board
-    private Piece piece = null;
+  //Space has no piece before setUpBoard() in Board
+  private Piece piece = null;
 
-    public Space(int row, int column){
-      this.row = row;
-      this.column = column;
-      setSpaceType();
-      setSpaceColor();
+  private boolean hasPiece;
+
+  public Space(int row, int column) {
+    this.row = row;
+    this.column = column;
+    setSpaceType();
+    setSpaceColor();
+
+  }
+
+  /**
+   * Sets the Space type based on row.
+   */
+  private void setSpaceType() {
+    if (row == 0) {
+      this.spaceType = SpaceType.KING_BLACK;
+    } else if (row == 7) {
+      this.spaceType = SpaceType.KING_RED;
+    } else {
+      this.spaceType = SpaceType.NON_KING;
     }
+  }
 
-    /*
-    Sets the Space type based on row.
-     */
-    private void setSpaceType(){
-      if(row == 0){
-        this.spaceType = SpaceType.KING_BLACK;
-      }
-      else if (row == 7){
-        this.spaceType = SpaceType.KING_RED;
-      }
-      else{
-        this.spaceType = SpaceType.NON_KING;
-      }
+  /**
+   * Sets the Space color based on rows and columns.
+   */
+  private void setSpaceColor() {
+    if (row % 2 == column % 2) {
+      spaceColor = SpaceColor.WHITE;
+    } else {
+      spaceColor = SpaceColor.BLACK;
     }
+  }
 
-    /*
-    Sets the Space color based on rows and columns.
-     */
-    private void setSpaceColor(){
-      if(row%2 == column%2){
-        spaceColor = SpaceColor.WHITE;
-      }
-      else{
-        spaceColor = SpaceColor.BLACK;
-      }
+  /**
+   * Checks the color of the space.
+   * Returns false for dark square and true for light square.
+   *
+   * @return indicator of color in boolean
+   */
+  public boolean checkColor() {
+    if (row % 2 == 0) {
+      return column % 2 == 0;
+    } else {
+      return column % 2 == 1;
     }
+  }
 
-    /**
-     * Checks the color of the space. Returns false for dark square and true for light square.
-     *
-     * @return indicator of color in boolean
-     */
-    public boolean checkColor(){
-        if(row%2==0) {
-            return column % 2 == 0;
-        }
-        else{
-            return column % 2 == 1;
-        }
-    }
-    
-    public boolean hasChecker(){
-        return this.piece != null;
-    }
+  /**
+   * Checks if the space is a valid space to move to.
+   * @return true if this space is valid, false otherwise.
+   */
+  public boolean isValid() {
+    return this.spaceColor == SpaceColor.BLACK && !this.hasPiece;
+  }
 
-    public Piece getPiece(){
-        return piece;
-    }
+  public boolean hasChecker() {
+    return this.piece != null;
+  }
 
-    public void setPiece(Piece piece){
-        this.piece = piece;
-    }
+  public Piece getPiece() {
+    return piece;
+  }
+
+  public void setPiece(Piece piece) {
+    this.piece = piece;
+  }
 }
