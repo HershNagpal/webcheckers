9bc7c25b75ecc4b91d81a7831550d41c51e57f6f
lxg8800 <lxg8800@rit.edu>
2018-10-12 17:31:04 -0400

    getBoardView() in Board class

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -13,11 +13,11 @@ public class Board {
     private static int ROWS = 8;
     private static int COLUMNS = 8;
     private Piece[][] pieces;
-    private BoardView board;
+    private BoardView boardView;
 
     public Board(){
         pieces = new Piece[ROWS][COLUMNS];
-        board = new BoardView(pieces);
+        boardView = new BoardView(pieces);
         setUpBoard();
     }
 
@@ -41,5 +41,9 @@ public class Board {
                 }
             }
         }
+    }
+
+    public BoardView getBoardView(){
+      return boardView;
     }
 }
