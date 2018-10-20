1d94a114ca5893153e03a9453dc46e5f46a942a0
mxk5025 <michaelkha213@gmail.com>
2018-10-20 16:51:32 -0400

    Stubbing GetHomeRouteTest setup

--- src/test/java/com/webcheckers/ui/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/ui/GetHomeRouteTest.java
@@ -1,4 +1,39 @@
 package com.webcheckers.ui.ui;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.ui.GetHomeRoute;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+import spark.TemplateEngine;
+
+import static org.mockito.Mockito.mock;
+
+@Tag("UI-tier")
 public class GetHomeRouteTest {
+
+    private GetHomeRoute CuT;
+
+    private PlayerLobby playerLobby;
+
+    // mock objects
+    private Request request;
+    private Session session;
+    private TemplateEngine engine;
+    private Response response;
+
+
+    @BeforeEach
+    public void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+
+        response = mock(Response.class);
+        engine = mock(TemplateEngine.class);
+
+        playerLobby = new PlayerLobby();
+        CuT = new GetHomeRoute(playerLobby, engine);
+    }
 }
