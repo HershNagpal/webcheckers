b90f4ddfdeafac732e180d0e869f709895c1dd81
mxk5025 <mxk5025@rit.edu>
2018-10-22 10:13:17 -0400

    Stubbed PlayerLobby tests

--- src/test/java/com/webcheckers/appl/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/appl/PlayerLobbyTest.java
@@ -1,4 +1,47 @@
 package com.webcheckers.appl;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Michael Kha
+ */
+@Tag("Application-tier")
 public class PlayerLobbyTest {
+
+    @Test
+    public void testValidUsername() {
+
+    }
+
+    @Test
+    public void testUsernameTaken() {
+
+    }
+
+    @Test
+    public void testSignIn() {
+
+    }
+
+    @Test
+    public void testSignOut() {
+
+    }
+
+    @Test
+    public void testGetPlayer() {
+
+    }
+
+    @Test
+    public void testGetPlayerLobbyNames() {
+
+    }
+
+    @Test
+    public void testSize() {
+
+    }
 }
