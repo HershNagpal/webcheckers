83c833e123821101118d46a4c55d6d2904c21f54
mxk5025 <mxk5025@rit.edu>
2018-10-25 14:30:46 -0400

    Revising classes to follow uml

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -13,34 +13,11 @@ public class Move {
         this.end = end;
     }
 
-    /**
-     *
-     */
-    public void simpleMove() {
-        if (canMove()) {
-            Piece piece = start.getPiece();
-            start.removePiece();
-            end.setPiece(piece);
-        }
+    public Position getStart() {
+        return start;
     }
 
-    /**
-     *
-     */
-    public void jumpMove() {
-
+    public Position getEnd() {
+        return end;
     }
-
-    /**
-     * Can move if the following are true:
-     * 1. There is a piece on start
-     * 2. There is a no piece on end
-     * 3. The direction between spaces is diagonal
-     * 4. The piece must move forward
-     * @return Can a piece move?
-     */
-    public boolean canMove() {
-        return true;
-    }
-
 }
--- src/main/java/com/webcheckers/model/Position.java
+++ src/main/java/com/webcheckers/model/Position.java
@@ -11,42 +11,19 @@ public class Position {
      * Coordinates on the board
      */
     private int row;
-    private int col;
+    private int cell;
 
-    /**
-     * The piece at this position
-     */
-    private Piece piece;
-
-    public Position(int row, int col, Piece piece) {
+    public Position(int row, int cell) {
         this.row = row;
-        this.col = col;
-        this.piece = piece;
+        this.cell = cell;
     }
 
     public int getRow() {
         return row;
     }
 
-    public int getCol() {
-        return col;
+    public int getCell() {
+        return cell;
     }
-
-    public Piece getPiece() {
-        return piece;
-    }
-
-    public void setPiece(Piece piece) {
-        this.piece = piece;
-    }
-
-    public void removePiece() {
-        this.piece = null;
-    }
-
-    public boolean hasPiece() {
-        return piece != null;
-    }
-
 
 }
