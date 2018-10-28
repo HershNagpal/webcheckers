37414b46f0b6a269c54ec63a439457aa9b8c561f
mxk5025 <michaelkha213@gmail.com>
2018-10-28 12:45:08 -0400

    Documenting

--- src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
@@ -19,9 +19,22 @@ import java.util.Objects;
  */
 public class PostCheckTurnRoute implements Route {
 
+    /**
+     * The game center
+     */
     private GameCenter gameCenter;
+
+    /**
+     * Interprets and converts json
+     */
     private Gson gson;
 
+    /**
+     * The constructor for the {@code POST /game} route handler.
+     *
+     * @param gameCenter holds the ongoing games
+     * @param gson used to interpret and convert json
+     */
     public PostCheckTurnRoute(GameCenter gameCenter, Gson gson) {
         Objects.requireNonNull(gameCenter, "gameCenter must not be null");
         Objects.requireNonNull(gson, "gson must not be null");
@@ -30,12 +43,12 @@ public class PostCheckTurnRoute implements Route {
         this.gson = gson;
     }
 
-  /**
-   * This action checks to see if the opponent has submitted their turn.
-   * @param request The HTTP request
-   * @param response The HTTP response
-   * @return Info Message, true if opponent is done with turn, false otherwise.
-   */
+    /**
+     * This action checks to see if the opponent has submitted their turn.
+     * @param request The HTTP request
+     * @param response The HTTP response
+     * @return Info Message, true if opponent is done with turn, false otherwise.
+     */
     @Override
     public Object handle(Request request, Response response) {
         Session session = request.session();
