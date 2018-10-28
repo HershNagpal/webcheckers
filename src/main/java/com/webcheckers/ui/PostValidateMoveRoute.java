2b8761f0ab8382ff29fb4ca6150f15a8d48154dd
mxk5025 <michaelkha213@gmail.com>
2018-10-28 12:50:58 -0400

    Documenting

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -15,9 +15,24 @@ import java.util.Objects;
  */
 public class PostValidateMoveRoute implements Route {
 
+    /**
+     * The game center
+     */
     private GameCenter gameCenter;
+
+    /**
+     * Interprets and converts json
+     */
     private Gson gson;
 
+    /**
+     * The constructor for the {@code POST /game} route handler to submit
+     * the players turn. A turn consists of a valid move and changing whose
+     * turn it is.
+     *
+     * @param gameCenter holds the ongoing games
+     * @param gson used to interpret and convert json
+     */
     public PostValidateMoveRoute(GameCenter gameCenter, Gson gson) {
         Objects.requireNonNull(gameCenter, "gameCenter must not be null");
         Objects.requireNonNull(gson, "gson must not be null");
@@ -26,6 +41,12 @@ public class PostValidateMoveRoute implements Route {
         this.gson = gson;
     }
 
+    /**
+     * Handles the request to validate a move.
+     * @param request The HTTP request
+     * @param response The HTTP response
+     * @return Message, error if invalid move, info if valid move
+     */
     @Override
     public Object handle(Request request, Response response) {
         String moveJSON = request.body();
