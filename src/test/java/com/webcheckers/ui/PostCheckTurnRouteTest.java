072f35fb5f29499b1fef39049c521913b0819892
mxk5025 <mxk5025@rit.edu>
2018-10-31 10:28:58 -0400

    Writing tests

--- src/test/java/com/webcheckers/ui/PostCheckTurnRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostCheckTurnRouteTest.java
@@ -1,8 +1,18 @@
 package com.webcheckers.ui;
 
+import com.google.gson.Gson;
+import com.webcheckers.appl.GameCenter;
+import com.webcheckers.model.Game;
+import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -14,30 +24,48 @@ public class PostCheckTurnRouteTest {
     /**
      * Component under test
      */
-
+    private PostCheckTurnRoute CuT;
 
     /**
      * friendly objects
      */
-
+    private Gson gson;
+    private GameCenter gameCenter;
 
     /**
      * mock objects
      */
-
+    private Request request;
+    private Response response;
+    private Session session;
+    private Player player;
+    private Game game;
 
     @BeforeEach
     public void setup() {
+        request = mock(Request.class);
+        response = mock(Response.class);
+        session = mock(Session.class);
+        player = mock(Player.class);
+        game = mock(Game.class);
 
+        gameCenter = new GameCenter();
+        gson = new Gson();
+
+        CuT = new PostCheckTurnRoute(gameCenter, gson);
     }
 
     @Test
     public void testIsYourTurn() {
+        when(game.isActivePlayer(player)).thenReturn(true);
+        when(gameCenter.getGame(player)).thenReturn(game);
 
     }
 
     @Test
     public void testNotYourTurn() {
+        when(game.isActivePlayer(player)).thenReturn(false);
+        when(gameCenter.getGame(player)).thenReturn(game);
 
     }
 }
