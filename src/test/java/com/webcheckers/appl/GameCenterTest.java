e99b1eef09a447be528f125c1946c0a0774769f2
mxk5025 <mxk5025@rit.edu>
2018-10-31 10:17:31 -0400

    Stubbed move branch tests

---
+++ src/test/java/com/webcheckers/appl/GameCenterTest.java
@@ -0,0 +1,44 @@
+package com.webcheckers.appl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author
+ */
+@Tag("Application-tier")
+public class GameCenterTest {
+
+    /**
+     * Component under test
+     */
+
+
+    /**
+     * friendly objects
+     */
+
+
+    /**
+     * mock objects
+     */
+
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+}
