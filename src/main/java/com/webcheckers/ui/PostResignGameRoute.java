e9f893e5ba94c6f15d94c0218690655a8cb154a3
mxk5025 <michaelkha213@gmail.com>
2018-11-04 14:16:25 -0500

    Tests and docs

--- src/main/java/com/webcheckers/ui/PostResignGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostResignGameRoute.java
@@ -20,9 +20,23 @@ import java.util.Objects;
  */
 public class PostResignGameRoute implements Route {
 
+    /**
+     * The game center
+     */
     private GameCenter gameCenter;
+
+    /**
+     * Interprets and converts json
+     */
     private Gson gson;
 
+    /**
+     * The constructor for the {@code POST /game} route handler to resign
+     * from a game.
+     *
+     * @param gameCenter holds the ongoing games
+     * @param gson used to interpret and convert json
+     */
     public PostResignGameRoute(GameCenter gameCenter, Gson gson) {
         Objects.requireNonNull(gameCenter, "gameCenter must not be null");
         Objects.requireNonNull(gson, "gson must not be null");
@@ -31,6 +45,12 @@ public class PostResignGameRoute implements Route {
         this.gson = gson;
     }
 
+    /**
+     * This action tells the game that the current player is attempting to resign.
+     * @param request The HTTP request
+     * @param response The HTTP response
+     * @return Message indicating if the user successfully resigned or not
+     */
     @Override
     public Object handle(Request request, Response response) {
         Session session = request.session();
