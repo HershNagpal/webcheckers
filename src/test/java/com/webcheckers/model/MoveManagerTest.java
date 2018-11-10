8deb2e5c5bedb246e24f037a67e3dee589653294
lxg8800 <lxg8800@rit.edu>
2018-11-10 18:40:55 -0500

    Stubbed MoveManagerTest class.

---
+++ src/test/java/com/webcheckers/model/MoveManagerTest.java
@@ -0,0 +1,26 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test suite for the MoveManager class.
+ *
+ * @author Luis Gutierrez
+ */
+public class MoveManagerTest {
+
+    /**
+     * The objects to be tested.
+     */
+    private Game CuT;
+
+    /**
+     * Initialize the objects to test
+     */
+    @BeforeEach
+    public void setup(){
+
+    }
+}
