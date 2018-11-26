d51bd24da09082649baf10cc9c621dd9c3a702e8
Hersh Nagpal <hxn6993@rit.edu>
2018-11-26 15:26:28 -0500

    Cleaned up documentation in the Message class.

--- src/main/java/com/webcheckers/model/Message.java
+++ src/main/java/com/webcheckers/model/Message.java
@@ -7,24 +7,37 @@ package com.webcheckers.model;
 public class Message {
 
     /**
-     * text - the text for the message to display.
-     * type - the context of the message (informational or error).
+     * The text that this message contains.
      */
     private String text;
+
+    /**
+     * The type of this message (Error or Info).
+     */
     private MessageType type;
 
-    //
-    // Constructor
-    //
+    /**
+     * Construct a message.
+     */
     public Message(String text, MessageType type) {
         this.text = text;
         this.type = type;
     }
 
+    /**
+     * Return the text of this message.
+     * 
+     * @return the String text of this message.
+     */
     public String getText(){
         return this.text;
     }
 
+    /**
+     * Return whether this message is an error or info.
+     * 
+     * @return the MessageType of this message.
+     */
     public MessageType getType(){
         return this.type;
     }
