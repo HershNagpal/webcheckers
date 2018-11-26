8695f7c9d6bcb93d52a9343c215df5e8dd7d479b
lxg8800 <lxg8800@rit.edu>
2018-11-25 23:07:27 -0500

    Updated Acceptance Test plan and added custom board to test end game condition.

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -58,6 +58,16 @@ public class Board {
                     {null, null, null, null, null, null, null, null},
                     {null, null, null, null, null, null, null, null}
             };
+    public final static Piece[][] END_GAME02 =
+            {   {null, null, null, null, null, null, null, null},
+                    {null, null, null, null, null, null, null, null},
+                    {null, null, null, null, null, null, null, null},
+                    {null, null, null, null, null, null, null, null},
+                    {null, null, null, null, null, null, null, null},
+                    {red, null, red, null, null, null, null, null},
+                    {null, null, null, null, null, null, null, null},
+                    {white, null, null, null, null, null, null, null}
+            };
 
     /**
      * Board constructor that initializes and sets up the 2d Piece array.
